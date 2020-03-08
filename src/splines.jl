13fcbc21684b9b2b6ff6c38a0126d0b110e53d36
Felipe Noronha <felipenoris@gmail.com>
2020-03-08 00:29:39 -0300

    refactoring

--- src/splines.jl
+++ src/splines.jl
@@ -7,10 +7,10 @@ mutable struct Spline{T}
     y::Vector{Float64}
     params::Vector{Float64} # stores parameters for polynomials. [ a1, b1, c1, d1, a2, b2, c2, d2 ...]
 
-    function Spline{T}(x::Vector{T}, y::Vector{Float64}, params::Vector{Float64}) where {T}
+    function Spline(x::Vector{T}, y::Vector{Float64}, params::Vector{Float64}) where {T}
         polynms_count = length(x) - 1
         @assert length(params) == polynms_count * 4 "params length $(length(params)) does not conform to the expected number of polynomials ($(polynms_count))" # each polynomial has 4 parameters
-        new(x, y, params)
+        return new{T}(x, y, params)
     end
 end
 
@@ -48,7 +48,7 @@ function splineint(s::Spline{T}, x_out::T) where {T}
 end
 
 # Performs natural cubic spline interpolation
-function splineint(s::Spline{T}, x_out::Vector{T}) where {T}
+function splineint(s::Spline{T}, x_out::Vector{T}) :: Vector{Float64} where {T}
     len = length(x_out)
     y_out = Vector{Float64}(undef, len)
     for i in 1:len
@@ -58,7 +58,7 @@ function splineint(s::Spline{T}, x_out::Vector{T}) where {T}
 end
 
 # Build a Spline object by fitting 3rd order polynomials around points (x_in, y_in)
-function splinefit(x_in::Vector{T}, y_in::Vector{Float64}) where {T}
+function splinefit(x_in::Vector{T}, y_in::Vector{Float64}) :: Spline{T} where {T}
     #
     # TODO: optimize. See http://www.math.ntnu.no/emner/TMA4215/2008h/cubicsplines.pdf
     #
@@ -135,5 +135,5 @@ function splinefit(x_in::Vector{T}, y_in::Vector{Float64}) where {T}
     A[row, (points_count-2)*4 + 3] = 2.0
     A[row, (points_count-2)*4 + 4] = 6.0 * x_in[points_count]
 
-    return Spline{eltype(x_in)}(x_in, y_in, A \ b)
+    return Spline(x_in, y_in, A \ b)
 end
