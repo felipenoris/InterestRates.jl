e56f3e550c09c2831f05276e1243ab243375c587
Felipe Noronha <felipenoris@gmail.com>
2020-05-13 17:04:36 -0300

    complete example for CurveMap

--- src/curvemap.jl
+++ src/curvemap.jl
@@ -11,7 +11,20 @@ zero_rate(curve::CurveMap, maturity::Date) = curve.f(zero_rate(curve.curve))
 # Example
 
 ```julia
+vert_x = [11, 15, 19, 23]
+vert_y = [0.09, 0.14, 0.19, 0.18] # yield values 9%, 14%, 19%, 18%
 
+# parallel shock of 1%
+map_parallel_1pct = r -> r + 0.01
+
+dt_curve = Date(2015,08,03)
+
+curve_map = InterestRates.CurveMap(map_parallel_1pct, InterestRates.IRCurve("dummy-cont-flatforward", InterestRates.Actual360(),
+    InterestRates.ContinuousCompounding(), InterestRates.FlatForward(), dt_curve,
+    vert_x, vert_y))
+
+# will report zero rate as 10% for maturity 11 days
+zero_rate(curve_map, dt_curve + Dates.Day(11)) ≈ 0.1
 ```
 """
 struct CurveMap{M, C<:AbstractIRCurve{M}, F<:Function} <: AbstractIRCurve{M}
