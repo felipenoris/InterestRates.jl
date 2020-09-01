ec3dc58cef2572db5c052de6fc4bb1c62b1eecae
Felipe Noronha <felipenoris@gmail.com>
2020-09-01 14:25:00 -0300

    make BufferedCurve thread-safe

--- src/bufferedcurve.jl
+++ src/bufferedcurve.jl
@@ -3,9 +3,10 @@ mutable struct BufferedIRCurve{M, C<:AbstractIRCurve{M}} <: AbstractIRCurve{M}
     dates_buffer::Dict{Date, Float64}
     yf_buffer::Dict{Rational, Float64}
     curve::C
+    lock::ReentrantLock
 end
 
-BufferedIRCurve(curve::AbstractIRCurve) = BufferedIRCurve(Dict{Date, Float64}(), Dict{Rational, Float64}(), curve)
+BufferedIRCurve(curve::AbstractIRCurve) = BufferedIRCurve(Dict{Date, Float64}(), Dict{Rational, Float64}(), curve, ReentrantLock())
 
 for fun in (:curve_get_name, :curve_get_daycount, :curve_get_compounding, :curve_get_method, :curve_get_date, :curve_get_dtm, :curve_get_zero_rates, :curve_get_model_parameters, :curve_get_spline_fit_on_rates, :curve_get_spline_fit_on_discount_factors)
     @eval begin
@@ -16,18 +17,29 @@ end
 curve_get_dict_parameter(curve::BufferedIRCurve, sym::Symbol) = curve_get_dict_parameter(curve.curve, sym)
 
 function zero_rate(curve::BufferedIRCurve, maturity::Date)
-    if !haskey(curve.dates_buffer, maturity)
-        curve.dates_buffer[maturity] = _zero_rate(curve_get_method(curve), curve, maturity)
+    lock(curve.lock)
+    try
+        if !haskey(curve.dates_buffer, maturity)
+            curve.dates_buffer[maturity] = _zero_rate(curve_get_method(curve), curve, maturity)
+        end
+        return curve.dates_buffer[maturity]
+    finally
+        unlock(curve.lock)
     end
-    return curve.dates_buffer[maturity]
 end
 
 function zero_rate(curve::BufferedIRCurve, maturity::YearFraction{T}) where {T<:Union{Integer, Rational}}
     yf_value = value(maturity)
-    if !haskey(curve.yf_buffer, yf_value)
-        curve.yf_buffer[yf_value] = _zero_rate(curve_get_method(curve), curve, maturity)
+
+    lock(curve.lock)
+    try
+        if !haskey(curve.yf_buffer, yf_value)
+            curve.yf_buffer[yf_value] = _zero_rate(curve_get_method(curve), curve, maturity)
+        end
+        return curve.yf_buffer[yf_value]
+    finally
+        unlock(curve.lock)
     end
-    return curve.yf_buffer[yf_value]
 end
 
 function zero_rate(curve::BufferedIRCurve, maturity_vec::Vector{T}) where {T<:Union{Date, YearFraction}}
